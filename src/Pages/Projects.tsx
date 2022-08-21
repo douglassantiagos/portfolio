d2efc4f03788efacdfc53a9b912df3a9569c3bce
douglassantiagos <douglassantiagods@hotmail.com>
2022-08-21 20:31:09 -0300

    adding projects

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -11,7 +11,11 @@ export function Projects() {
           image="./pokedex-photo.png"
           title="Pokedex Project"
           subtitle="Aplicação com informações detalhadas de todos os pokemons do universo pokemon. Aplicação responsiva desenvolvido para web consumindo uma API externa."
-          tech="Typescript"
+          tech_pri="Typescript"
+          tech_sec="React"
+          tech_third="ChakraUI"
+          tech_fourth="Axios"
+          tech_fifth="Pokeapi"
           linkVisit="https://pokedex-project-reactjs.vercel.app/"
           linkGithub="https://github.com/douglassantiagos/pokedex_project_reactjs"
         />
@@ -19,10 +23,31 @@ export function Projects() {
         <Project
           image="./ignews-photo.png"
           title="Ignews"
-          subtitle="Aplicação com informações detalhadas de todos os pokemons do universo pokemon. Aplicação responsiva desenvolvido para web consumindo uma API externa."
-          tech="Typescript"
+          subtitle="O ignews é um blog de conteúdos com status de assinatura.
+          O blog possui um sistema de compra integrado com STRIPE. Ao realizar o pagamento, a inscrição estará ativa para acesso completo do blog. 
+          Caso contrário, o usuário terá acesso limitado aos conteúdos."
+          tech_pri="Typescript"
+          tech_sec="React"
+          tech_third="Next.js"
+          tech_fourth="Prismic"
+          tech_fifth="Fauna-db"
+          tech_sixth="Stripe"
           linkVisit="https://ignews-swart-mu.vercel.app/"
           linkGithub="https://github.com/douglassantiagos/Ignews"
+        />
+
+        <Project
+          image="./jobscalc-photo.png"
+          title="JobsCalc"
+          subtitle="O JobsCalc é uma aplicação de estimativa de cálculo para projetos freelancer, onde é possível cadastrar e excluir jobs (projetos), obtendo uma estimativa de custo de cada job. Além disso, é possível traçar o valor da hora da pessoa que estará usando o sistema."
+          tech_pri="Javascript"
+          tech_sec="NodeJS"
+          tech_third="EJS"
+          tech_fourth="Express"
+          tech_fifth="SQLite"
+          tech_sixth="HTML/CSS"
+          linkVisit="https://jobscalc-beta.vercel.app/"
+          linkGithub="https://github.com/douglassantiagos/Maratona-JobsCalc"
         />
       </section>
     </div>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,17 +5,34 @@ interface ProjectProps {
   image: string;
   title: string;
   subtitle: string;
-  tech: string;
+  tech_pri: string;
+  tech_sec: string;
+  tech_third: string;
+  tech_fourth: string;
+  tech_fifth: string;
+  tech_sixth?: string;
   linkVisit: string;
   linkGithub: string;
 }
 
-export function Project({ image, title, subtitle, tech, linkVisit, linkGithub }: ProjectProps) {
+export function Project({ 
+  image, 
+  title, 
+  subtitle, 
+  tech_pri, 
+  tech_sec, 
+  tech_third, 
+  tech_fourth, 
+  tech_fifth,
+  tech_sixth, 
+  linkVisit, 
+  linkGithub 
+}: ProjectProps) {
   return (
     <div className="bg-purple-900 flex flex-row min-h-min" >
       <img src={image} className="w-1/2" />
 
-      <div className="m-10 space-y-5">
+      <div className="m-6 space-y-5">
         <h1 className="text-2xl font-bold">
           {title}
         </h1>
@@ -23,7 +40,12 @@ export function Project({ image, title, subtitle, tech, linkVisit, linkGithub }:
           {subtitle}
         </p>
         <ul className="flex font-Roboto text-gray-400 text-sm space-x-5 ">
-          <li>{tech}</li>
+          <li>{tech_pri}</li>
+          <li>{tech_sec}</li>
+          <li>{tech_third}</li>
+          <li>{tech_fourth}</li>
+          <li>{tech_fifth}</li>
+          <li>{tech_sixth}</li>
         </ul>
         <div className="flex space-x-5">
           <Button title='Visit' href={linkVisit} />
