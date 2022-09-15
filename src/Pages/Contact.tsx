6cd15a0120c4b24fbbb23b860e7cd6ac983d4eae
douglassantiagos <douglassantiagods@hotmail.com>
2022-09-14 21:12:43 -0300

    design finished

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -25,7 +25,7 @@ export function Contact() {
       id="contact" 
       initial="offscreen"
       whileInView="onscreen"
-      viewport={{ once: true, amount: 0.6 }} 
+      viewport={{ once: true, amount: 0.5 }} 
       className="flex flex-col min-h-screen max-auto 2xl:mt-10 items-center justify-center space-y-5 dark:bg-gradient-to-br dark:from-[#FF9900] dark:to-[#FF7C00]"
     >
       <motion.div variants={Variants}  className="flex flex-col flex-wrap sm:max-w-sm items-center justify-center space-y-5 md:my-32 p-10 sm:p-4 border-[1px] dark:border-white rounded-xl border-purple-600">
