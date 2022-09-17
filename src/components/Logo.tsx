3c07073ff7c7c8c393d4310a0fc0624a3fb15408
douglassantiagos <douglassantiagods@hotmail.com>
2022-09-17 11:57:39 -0300

    finished design details

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -16,7 +16,7 @@ export function Logo() {
   return (
     <motion.div variants={container} initial="hidden" animate="visible" className="md:absolute 2xl:fixed fixed top-12 sm:top-8 left-36 2xl:left-20 lg:left-10 sm:left-6">
       <a href="/">
-        <span className="self-start text-3xl px-3 sm:px-2 py-2 sm:py-1 border-2 border-l-black border-t-black rounded-lg rounded-tr-none rounded-bl-none font-bold border-purple-600 dark:border-l-[#FF7C00] dark:border-t-[#FF7C00] dark:border-purple-900">
+        <span className="self-start text-3xl px-2 py-1 border-2 border-l-black border-t-black rounded-lg rounded-tr-none rounded-bl-none font-bold border-purple-600 dark:border-l-[#FF7C00] dark:border-t-[#FF7C00] dark:border-purple-900">
           D
         </span>
       </a>
